ace535beee23226a6bf3fffbceaccb1fa3da6f1c
aaronisanoobie <167141925+aaronisanoobie@users.noreply.github.com>
2024-04-15 19:21:17 -0400

    Release calculator
    Crappy calculator, works ig, only does addition

---
+++ Calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int x, y;
+    int sum;
+    cout << "This is  a calc!\n";
+    cout << "Whats the first number?\n";
+    cin >> x;
+    cout << "Cool your first number is " << x;
+    cout << "\nWhat is the second number?\n";
+    cin >> y;
+    cout << "Cool your second number is " << y;
+    sum = x + y;
+    cout << "\nYour answer is... \n" << sum;
+  return 0;
+}
